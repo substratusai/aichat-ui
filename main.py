b4b6295191f933c2023f1a66d47531b51cfbd960
Sam Stoelinga <sammiestoel@gmail.com>
2024-02-23 21:48:14 -0800

    add api key option

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 default_api_host = os.environ.get("API_HOST", "http://localhost:8080/v1")
 default_model = os.environ.get("MODEL", "mixtral-8x7b-instruct-gptq")
+default_api_key = os.environ.get("API_KEY", "Ignored by most backends. Can be anything.")
 
 st.title("AI Chat UI")
 
@@ -22,8 +23,9 @@ with st.sidebar:
     st.title("Settings")
     st.text_input("API Host", key="api_host", value=default_api_host)
     st.text_input("Model", key="model", value=default_model)
+    st.text_input("API Key (Optional)", key="api_key", value=default_api_key)
 
-client = OpenAI(api_key="canbeanything", base_url=st.session_state.api_host)
+client = OpenAI(api_key=st.session_state.api_key, base_url=st.session_state.api_host)
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -32,7 +34,6 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What is up?"):
     # Remove the previous messages from user if there is no assist response in between
     i = len(st.session_state.messages) - 1
-    print(i)
     while i >= 0:
         if st.session_state.messages[i]["role"] == "user":
             print("Popping")
@@ -47,7 +48,6 @@ if prompt := st.chat_input("What is up?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        print(st.session_state.messages)
         stream = client.chat.completions.create(
             model=st.session_state["model"],
             messages=[
